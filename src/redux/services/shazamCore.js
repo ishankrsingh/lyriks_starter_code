4a5bac54b88e4d0178ebf7cf06dfce3cfa13f15a
ishankrsingh <ishankrsingh@gmail.com>
2022-10-02 00:30:15 +0530

    Rapid API Music Fetching

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,46 @@
+//This is gonna be our file where we will be making API calls and we will be making use of 'RapidAPI client' extension
+
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'; //two utility functions imported from redux toolkit
+
+/* Below code is directly copied from RapidAPI client extension
+
+
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '5697e54fabmsh5f47e9905b6208ap1ee6a9jsnf1e3abe3d673',
+      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+*/
+
+
+    //reducerPath will store the name of our API
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl:'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders:(headers)=>{
+
+            headers.set('X-RapidAPI-Key','5697e54fabmsh5f47e9905b6208ap1ee6a9jsnf1e3abe3d673');
+            //It is going to prepare headers for us before each and every API call so that we don't have to do it every time.
+
+            return headers;
+
+
+        },
+    }),
+    endpoints: (builder)=>({
+        getTopCharts:builder.query({query: () =>'/charts/world'}),
+
+    }),
+});
+
+export const{
+    useGetTopChartsQuery,
+}=shazamCoreApi;
